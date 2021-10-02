6cd11069bfb6261b461913b92336b1343d8a0258
Rahul Dodda <rahul.dodda@infor.com>
2021-10-02 09:38:06 +0530

    linked list in progress

--- Linked-Lists/remove_duplicates.py
+++ Linked-Lists/remove_duplicates.py
@@ -18,18 +18,48 @@ ll_instance.display()
 
 def remove_duplicates(head: LinkedList):
     hash_map = set()
-
+    prevoius = None
     while head:
-        if head.data not in hash_map:
-            hash_map.add(head.data)
-            head = head.next
+        if head.data in hash_map:
+            prevoius.next = head.next
         else:
-            if head.next:
-                head.data = head.next.data
-                head.next = head.next.next
-            else:
-                break
+            hash_map.add(head.data)
+            prevoius = head
+        head = head.next
+
+
 remove_duplicates(ll_instance.head)
-print('After removing duplicates')
+print("After removing duplicates")
 
 ll_instance.display()
+
+
+ll_instance2 = LinkedList()
+
+ll_instance2.add(1)
+ll_instance2.add(2)
+ll_instance2.add(1)
+ll_instance2.add(3)
+ll_instance2.add(1)
+ll_instance2.add(2)
+ll_instance2.add(3)
+ll_instance2.add(3)
+
+ll_instance2.display()
+
+def remove_duplicates_no_buffer_allowed(head: LinkedList):
+    current = head
+
+    while current:
+        runner = current
+        while runner.next:
+            if runner.next.data == current.data:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        current = current.next
+remove_duplicates_no_buffer_allowed(ll_instance2.head)
+print("After removing duplicates")
+
+ll_instance2.display()
+
