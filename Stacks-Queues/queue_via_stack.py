3349843d48bed2615e0aade1f43ce6f961fe8291
Rahul Dodda <rahul.dodda@infor.com>
2021-10-05 17:08:51 +0530

    Stacks and Queues  in progress..

--- Stacks-Queues/queue_via_stack.py
+++ Stacks-Queues/queue_via_stack.py
@@ -28,3 +28,40 @@ for i in range(10):
 
 print(q.dequeu())
 
+class MyQueueOptimized:
+    def __init__(self) -> None:
+        self.stack_new = []
+        self.stack_old = []
+
+    def size(self):
+        return len(self.stack_new) + len(self.stack_old)
+    
+    def shuffle(self):
+        if not self.stack_old:
+            while self.stack_new:
+                self.stack_old.append(self.stack_new.pop())
+    
+    def peek(self):
+        self.shuffle()
+        return self.stack_old[-1]
+
+    def enqueue(self, data):
+        self.stack_new.append(data)
+
+    def dequeue(self):
+        self.shuffle()
+        return self.stack_old.pop()
+
+
+
+qq = MyQueueOptimized()
+
+qq.enqueue(10)
+qq.enqueue(9)
+print(qq.stack_new, qq.stack_old)
+print(qq.peek())
+qq.enqueue(11)
+print(qq.stack_new, qq.stack_old)
+qq.enqueue(12)
+
+
