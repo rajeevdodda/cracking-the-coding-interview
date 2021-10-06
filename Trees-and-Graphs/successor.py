38f54fcb3132b4ded34b8b2c0dc0c516d2102aac
Rahul Dodda <rahul.dodda@infor.com>
2021-10-06 23:05:00 +0530

    Trees abd Graphs in progress..

--- Trees-and-Graphs/successor.py
+++ Trees-and-Graphs/successor.py
@@ -1,6 +1,7 @@
-# Write an algorithm to find the "next" node (i.e., in-order successor) of a 
-# given node in a binary tree. You may assume each nod ehas link to parent 
+# Write an algorithm to find the "next" node (i.e., in-order successor) of a
+# given node in a binary tree. You may assume each nod ehas link to parent
 # node
+
 
 class Node:
     def __init__(self, data) -> None:
@@ -23,3 +24,34 @@ root.right.right = Node(8)
 #                   (5)
 #           (3)             (7)
 #       (2)    (4)      (6)     (8)
+
+
+def find_successor(root, element):
+    successor = None
+    while root.data != element:
+        if root.data > element:
+            successor = root
+            root = root.left
+        else:
+            root = root.right
+
+    if root.right:
+        root = root.right
+        while root.left:
+            root = root.left
+
+        return root.data
+    else:
+        if successor:
+            return successor.data
+        else:
+            return None
+
+
+print(find_successor(root, 2))
+print(find_successor(root, 3))
+print(find_successor(root, 4))
+print(find_successor(root, 5))
+print(find_successor(root, 8))
+
+
